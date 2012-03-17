c57b465558365dc674bbd52886b0121c301fa3f9
stef <stefan.marsiske@gmail.com>
2012-03-17 22:19:06 +0100

    [enh] agents now listen as tor hidden services for p2p

--- macc.sh
+++ macc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/ksh
 
-# depends on apg, seccure
+# depends on apg, seccure, socat
 
 alias enc='seccure-encrypt -q -c p521 -m 256'
 alias dec='seccure-decrypt -q -c p521 -m 256'
@@ -9,14 +9,42 @@ alias verify='seccure-verify -c p521'
 alias key='seccure-key -q -c p521'
 alias dh='seccure-dh -q -c p521'
 
-PUBF="pub"
-KEYF="key"
+PORT=$(cat port)
+ONION=$(cat hostname)
 MULTIPLEXER="${1:-server}"
 VOLATILE="volatile"
+# for PoC purposes only, should be in volatile memory
+[[ ! -d "$VOLATILE" ]] && mkdir "$VOLATILE"
+
+PUBF="pub"
+KEYF="key"
+if [[ ! -r $KEYF ]]; then
+    # generate static private/pub key pair
+    echo "[!] no keys found, generating new" >&2
+    apg -q -a1 -m 90 -n 1 >"$KEYF"
+    key -F "$KEYF" >"$PUBF" 2>/dev/null
+    echo -n "[!] please share this public key with your peers "
+    cat "$PUBF"
+    echo "and populate with their keys your 'peers' file"
+fi
+
+PUB=$(cat "$PUBF")
+KEY=$(cat "$KEYF")
+
+# for p2p connections
+socket=$(mktemp)
+
+# dumps anything comming from the port to the socket file
+(socat -u tcp4-listen:$PORT,reuseaddr,fork,bind=127.0.0.1 open:"$socket",append,creat) &
+socatpid=$!
+sleep 0.5
+
+# step 1. announce intent to join on broadcast channel
+echo "agent:$ONION" >>"$MULTIPLEXER"/in
 
 # step 2. initiates a session setup with a new agent
 function agent {
-    [[ "x$1" == "x$socket" ]] && return
+    [[ "x$1" == "x$ONION" ]] && return
     printf "%s -!- %s found\n" "$(date '+%H:%M')" "$1"
     dh 2>&1 |&
     read -p p
@@ -25,7 +53,7 @@ function agent {
         read -p p
     done
     # step 3. send dh request to newly joined agent
-    sendto "$1" "dh:$socket:$p"
+    sendto "$1" "dh:$ONION:$p"
     tail -fn0 "$socket" | while read line; do
         echo "$line" | grep -qs "dh2:$1:" &&  {
             # step 6. finish dh exchange with joining agent
@@ -56,7 +84,7 @@ function dhreply {
     read -p vkey
     print "$skey\n$vkey" >"${VOLATILE}/${peer##*/}"
     # step 5. send dh reply to other agent
-    sendto "$peer" "dh2:$socket:$p2"
+    sendto "$peer" "dh2:$ONION:$p2"
     sleep 0.2
     # step 7. initiate authentication to other agent
     auth_reply "$peer" "$vkey" "auth"
@@ -84,7 +112,7 @@ function auth {
 # params: peer, vkey, cmd
 function auth_reply {
     auth=$(echo "$2" | sign -F "$KEYF" -a 2>&1 | fgrep -v "WARNING: Cannot obtain memory lock: Cannot allocate memory.")
-    sendto "$1" "$(printf "$3:$socket:"; echo "$auth" | openssl enc -aes-256-cfb -e -a -kfile "${VOLATILE}/${1##*/}" | tr -d '\n'; echo)"
+    sendto "$1" "$(printf "$3:$ONION:"; echo "$auth" | openssl enc -aes-256-cfb -e -a -kfile "${VOLATILE}/${1##*/}" | tr -d '\n'; echo)"
 }
 
 # initialization functions end
@@ -94,7 +122,7 @@ function auth_reply {
 function send {
     mkey=$(mktemp "$VOLATILE/key.XXXXXX")
     apg -q -a1 -m 90 -n 1 >"$mkey"
-    data=$(echo "msg:$socket:$1" | openssl enc -aes-256-cfb -e -a -kfile "$mkey" | tr -d '\n')
+    data=$(echo "msg:$ONION:$1" | openssl enc -aes-256-cfb -e -a -kfile "$mkey" | tr -d '\n')
     keybag=""
     sort "$VOLATILE/session" | uniq | while read peer; do
         keybag="$keybag $(cat "$mkey" | openssl enc -aes-256-cfb -e -a -kfile "${VOLATILE}/${peer##*/}" | tr -d '\n')"
@@ -117,7 +145,7 @@ function msg {
             clr="$(echo "$data" | sed 's/\(.\{64\}\)/\1\n/g' | openssl enc -aes-256-cfb -d -a -kfile "$mkey")"
             [[ "msg:" == "$(echo "$clr" | cut -c1-4)" ]] && {
                 peer=$(echo "${clr}" | cut -d':' -f2)
-                [[ "x$peer" == "x${socket}" ]] || {
+                [[ "x$peer" == "x${ONION}" ]] || {
                     msg=$(echo "${clr}" | cut -d':' -f3-)
                     printf "%s <%10-s> %s\n" "$(date '+%H:%M')" "$(peername $peer)" "$msg"
                 }
@@ -131,7 +159,7 @@ function msg {
 
 # leave a group
 function leave {
-    [[ "x$1" == "x$socket" ]] && return
+    [[ "x$1" == "x$ONION" ]] && return
     tmp=$(mktemp)
     sort  "$VOLATILE/session" | uniq | fgrep -v "${VOLATILE}/${1##*/}" >"$tmp" && mv "$tmp" "$VOLATILE/session"
     printf "%s -!- %10-s left\n"  "$(date '+%H:%M')" "$(peername $1)"
@@ -140,7 +168,7 @@ function leave {
 # helper functions
 
 function sendto {
-    echo "$2" >>"$1"
+    echo "$2" | socat -u - SOCKS4a:localhost:"$1":80,socksport=9050
 }
 
 function peername {
@@ -150,9 +178,9 @@ function peername {
 
 # clean up bg processes and volatile data
 function cleanup {
-    echo "leave:$socket" >>"$MULTIPLEXER"/in
+    echo "leave:$ONION" >>"$MULTIPLEXER"/in
     rm "$socket" "${VOLATILE}"/tmp.* "${VOLATILE}"/key.* "$VOLATILE"/session 2>>/dev/null
-    kill $groupdesc $p2pdesc #$userdesc
+    kill $groupdesc $p2pdesc $socatpid
     trap "-" INT PIPE EXIT HUP
     exit 2
 }
@@ -183,35 +211,13 @@ function p2p_dispatcher {
     done
 }
 
-# for p2p connections
-socket=$(mktemp)
-
-if [[ ! -r $KEYF ]]; then
-    # generate static private/pub key pair
-    echo "[!] no keys found, generating new" >&2
-    apg -q -a1 -m 90 -n 1 >"$KEYF"
-    key -F "$KEYF" >"$PUBF" 2>/dev/null
-    echo -n "[!] please share this public key with your peers "
-    cat "$PUBF"
-fi
-
-PUB=$(cat "$PUBF")
-KEY=$(cat "$KEYF")
-
-# for PoC purposes only, should be in volatile memory
-[[ ! -d "$VOLATILE" ]] && mkdir "$VOLATILE"
-
-# step 1. announce intent to join on broadcast channel
-echo "agent:$socket" >>"$MULTIPLEXER"/in
-
 # start handlers
 group_dispatcher &
 groupdesc=$!
 p2p_dispatcher &
 p2pdesc=$!
 
-# wait for user input
+# send for user input
 while read line; do
     send "$line"
 done
-
